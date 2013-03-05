8673fd2de46b641d130435ba6f37efc77ee51df5
Julio Delgado <juliom6@gmail.com>
2013-03-04 23:15:10 -0500

    Avanzando las vistas

--- search/views.py
+++ search/views.py
@@ -12,4 +12,13 @@ class Buscador(TemplateView):
 #    html = []
 #    for k, v in values:
 #        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+#
 #    return HttpResponse('<table>%s</table>' % '\n'.join(html))
+
+def resultados(request):
+	cine = request.GET['cine']
+	pelicula = request.GET['pelicula']
+	distrito = request.GET['distrito']
+	horario = request.GET['horario']
+	mensaje = "%r %r %r %r" % (cine, pelicula, distrito, horario)
+	return HttpResponse(mensaje)
--- sitio/urls.py
+++ sitio/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from search.views import Buscador
+from search.views import Buscador, resultados
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
 	#url(r'^cartelera/$', display_meta),
 
 	#url(r'^resultados/$', Buscador.resultados()),
+	url(r'^resultados/$', resultados),
 )
