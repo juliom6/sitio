1568c0994ef295bb8e2df8a729278a529ff9783d
Julio Delgado <juliom6@gmail.com>
2013-03-05 03:00:30 -0500

    Implementando las vistas

--- search/views.py
+++ search/views.py
@@ -1,26 +1,23 @@
 from django.views.generic import TemplateView
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from  search.models import Funcion
+from  search.models import Funcion, Pelicula
 
 class Buscador(TemplateView):
 	template_name = "cartelera.html"
 	#def resultados():
 	#	template_name = "cartelera.html"
 
-#def display_meta(request):
-#    values = request.META.items()
-#    values.sort()
-#    html = []
-#    for k, v in values:
-#        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
-#
-#    return HttpResponse('<table>%s</table>' % '\n'.join(html))
-
 def resultados(request):
+	error = False
 	cine = request.GET['cine']
 	pelicula = request.GET['pelicula']
 	distrito = request.GET['distrito']
 	horario = request.GET['horario']
-	funciones = Funcion.objects.filter()
-	return HttpResponse()
+	p = Pelicula.objects.get(titulo = pelicula)
+	q = p.funcion_set.filter()
+	if q:
+		return render_to_response('resultados.html', {'funciones': q})
+	else:
+		error = True
+		return render_to_response('cartelera.html', {'error': error})
